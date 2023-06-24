ca86c69f15f91ded074094b8f103d8bfaf5e5911
unknown <chandru151998@gmail.com>
2023-06-24 14:25:09 +0530

    [Chandru]Added. data structure program

---
+++ sorting/quick_sort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([4, 5, 1, 3, 2, 6]))
